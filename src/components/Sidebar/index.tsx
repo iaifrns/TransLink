aa0d58b20169f110bf10219ae2a6f677a3bd90c8
iaifrns <nsinifranc@gmail.com>
2025-06-16 14:24:13 +0100

    finished the modifications

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -348,7 +348,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 <NavLink
                   to={routesPath.ENTERPRISES}
                   className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes('calendar') &&
+                    pathname == routesPath.ENTERPRISES &&
                     'bg-graydark dark:bg-meta-4'
                   }`}
                 >
